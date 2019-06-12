e4cfcc4c53c0c2c20b25b32059bcf6a8e50e33cc
Joseph Arhar <josepharhar@gmail.com>
2019-06-11 19:56:40 -0700

    more binary testing

--- test/genbinary.js
+++ test/genbinary.js
@@ -8,3 +8,5 @@ for (let i = 0; i < 255 * 255; i++) {
 const buffer = Buffer.from(hexStr, 'hex');
 
 fs.writeFileSync('binary', buffer);
+
+fs.writeFileSync('invalidutf8', Buffer.from('c0c1f5f6f7f8f9fafbfcfdfeff', 'hex'));
