feafb5495f9ac082b057baeb0a66387cc41ad847
Joseph Arhar <josepharhar@gmail.com>
2019-06-29 15:42:36 -0700

    started on localbranch

---
+++ src/localBranch.js
@@ -0,0 +1,18 @@
+class LocalBranch {
+  /**
+   * TODO make this only callable from static factory method
+   * @param {string} name
+   */
+  constructor(name) {
+    this._name = name;
+  }
+}
+exports.LocalBranch = LocalBranch;
+
+/** @type {!Map<string, !LocalBranch>} */
+const _localBranchCache = new Map();
+/**
+ * @param {string} name
+ */
+exports.getLocalBranch = async function(name) {
+}
