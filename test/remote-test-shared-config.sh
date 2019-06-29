95015a0a17852b750b9c2dceae04116f1b0b9fe8
Joseph Arhar <josepharhar@gmail.com>
2019-06-29 00:05:58 -0700

    fixed jcmp

--- test/remote-test-shared-config.sh
+++ test/remote-test-shared-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 jcmp() {
-  find $1 -type f -exec sh -c 'md5sum --tag "{}" ; stat --printf="%y\n" "{}" ;' \; | paste -d " " - -
+  find $1 -type f | sort | xargs -I {} sh -c 'md5sum --tag "{}" ; stat --printf "%y\n" "{}" ;' | paste -d " " - -
 }
