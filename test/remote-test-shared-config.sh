a3c5daea064a5cc0efa260882d9b30d658ee1f22
Joseph Arhar <josepharhar@gmail.com>
2019-06-29 09:16:06 -0700

    fixed portable jcmp

--- test/remote-test-shared-config.sh
+++ test/remote-test-shared-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 jcmp() {
-  TZ=UTC find $1 -type f | sort | xargs -I {} sh -c 'md5sum --tag "{}" ; stat --printf "%y\n" "{}" ;' | paste -d " " - -
+  find $1 -type f | sort | xargs -I {} sh -c 'md5sum --tag "{}" ; TZ=UTC stat --printf "%y\n" "{}" ;' | paste -d " " - -
 }
