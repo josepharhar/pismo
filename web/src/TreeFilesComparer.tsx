04225840d46de2eaf1dd52903ab7bb07540ae9bc
Joseph Arhar <josepharhar@gmail.com>
2019-09-15 12:12:51 -0700

    stuff

--- web/src/TreeFilesComparer.tsx
+++ web/src/TreeFilesComparer.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from 'react';
 import { GetTreesResponse, FileInfo } from './PismoTypes';
 import './TreeFilesComparer.css';
 import './DataGrid.css';
+//import { FixedSizeList } from 'react-window';
+import filesize from 'filesize';
 
 interface Props {
   getTreesResponse: GetTreesResponse;
@@ -15,8 +17,16 @@ class TreeFilesComparer extends React.Component<Props> {
     right?: FileInfo;
   }>;
 
+  state: {
+    expandedPaths: Set<string>;
+  };
+
   constructor(props: Props) {
     super(props);
+
+    this.state = {
+      expandedPaths: new Set()
+    };
 
     const {getTreesResponse, leftBranchName, rightBranchName} = props;
 
@@ -54,33 +64,89 @@ class TreeFilesComparer extends React.Component<Props> {
     }
   }
 
-  fileToCell(file: FileInfo|undefined, index: number, side: 'left'|'right'): ReactNode {
+  fileToCell(file: FileInfo|undefined, index: number, side: 'left'|'right', expanded: boolean): ReactNode {
     const key = `${index}-${side}`;
     if (!file) {
-      return <div key={key} className="datagrid-cell empty">
-        TODO delet this text
-      </div>;
+      return <div key={key} className="datagrid-cell empty"><div>file not present</div></div>;
     }
     const {path, mtimeS, mtimeNs, size, hash} = file;
 
-    const textContent = path;
     // TODO find a better way to display this than with a tooltip?
     const tooltip = `mtime: ${mtimeS}.${mtimeNs}, size: ${size}, hash: ${hash}`;
 
-    return <div className="datagrid-cell" title={tooltip}>
-      <button title="copy to other side">copy</button>
-      <button title="delete from this side">delete</button>
-      {textContent}
-    </div>;
+    if (!expanded) {
+      return (
+        <div className="datagrid-cell clip-overflow monospace" title={tooltip}>
+          <button title="copy to other side">copy</button>
+          <button title="delete from this side">delete</button>
+          {path}
+        </div>
+      );
+    }
+
+    // TODO show milliseconds in a way that makes sense
+    return (
+      <div className="datagrid-cell monospace">
+        <button title="copy to other side">copy</button>
+        <button title="delete from this side">delete</button>
+        <table className="table-borders">
+          <tbody>
+            <tr>
+              <td>path</td>
+              <td>{path}</td>
+            </tr>
+            <tr>
+              <td>modified time</td>
+              <td>{new Date(mtimeS * 1000).toISOString()}</td>
+            </tr>
+            <tr>
+              <td>size</td>
+              <td>{filesize(size)}</td>
+            </tr>
+            <tr>
+              <td>hash</td>
+              <td>{hash}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  toggleExpanded(path: string) {
+    const expandedPaths = this.state.expandedPaths;
+    if (expandedPaths.has(path)) {
+      expandedPaths.delete(path);
+    } else {
+      expandedPaths.add(path);
+    }
+    this.setState({
+      expandedPaths
+    });
   }
 
   render() {
     return <div className="datagrid">
       {this.rows.map((row, index)=> {
-        return <div className="datagrid-row">
-          {this.fileToCell(row.left, index, 'left')}
-          {this.fileToCell(row.right, index, 'right')}
-        </div>;
+        let path: string = '';
+        if (row.left)
+          path = row.left.path;
+        else if (row.right)
+          path = row.right.path;
+        if (!path)
+          return null;
+
+        const expanded = this.state.expandedPaths.has(path);
+
+        return (
+          <div className="datagrid-row">
+            <div className="datagrid-cell datagrid-cell-no-grow">
+              <button onClick={() => this.toggleExpanded(path)}>expand</button>
+            </div>
+            {this.fileToCell(row.left, index, 'left', expanded)}
+            {this.fileToCell(row.right, index, 'right', expanded)}
+          </div>
+        );
       })}
     </div>;
   }
